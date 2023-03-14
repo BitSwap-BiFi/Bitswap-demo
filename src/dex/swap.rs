77df602b6ccc79709c8cf02de41408ec503022a3
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-03-14 15:22:09 -0300

    Update swap.rs

--- src/dex/swap.rs
+++ src/dex/swap.rs
@@ -5,9 +5,19 @@ use secp256k1::rand::rngs::OsRng;
 use std::str::FromStr;
 
 fn create_swap () {
-//pparameters for swap happen
+//parameters for swap happen
 let amount = 1
 let value = 1000
-let fee swap = 0.05
-let fee spread = 0.1
+let swap_fee = 0.05
+let fee_spread = 0.1
 
+fn swap_accepted () {
+//swap accepted by conterparty
+let amount
+let value
+let fee
+let swap_fee
+let fee_spread
+let dlc
+
+fn swp_out
