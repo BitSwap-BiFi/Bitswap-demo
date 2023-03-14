820f9ab92ed7a8d13148d0247e9219c53fc67d08
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-03-14 15:06:34 -0300

    Update swap.rs

--- src/dex/swap.rs
+++ src/dex/swap.rs
@@ -1,6 +1,13 @@
 use std::rgb-core
 
-use rgb::
-use rgb::
+use rgb::{self, fungible::Amount, schema::constants::*, schema::scripts::*, util::Value};
+use secp256k1::rand::rngs::OsRng;
+use std::str::FromStr;
 
+fn create_swap () {
+//pparameters for swap happen
+let amount = 1
+let value = 1000
+let fee swap = 0.05
+let fee spread = 0.1
 
