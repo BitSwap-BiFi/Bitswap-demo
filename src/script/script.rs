7278d6717cd3961912b4372c921c9ce6d42843b5
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-04-08 08:28:08 -0300

    Update script.rs

--- src/script/script.rs
+++ src/script/script.rs
@@ -1,20 +1,92 @@
-OP_IF
-    // Check that both BTC and USDT inputs are present
-    #btc_input && #usdt_input &&
-    // Check that the BTC input amount is greater than 0
-    #btc_input.amount > 0 &&
-    // Check that the USDT input amount is greater than 0
-    #usdt_input.amount > 0 &&
-    // Check that the BTC and USDT inputs have the same issuer
-    #btc_input.issuer == #usdt_input.issuer &&
-    // Check that the BTC and USDT inputs have different owners
-    #btc_input.owner != #usdt_input.owner
-OP_THEN
-    // Calculate the exchange rate
-    #rate = #usdt_input.amount / #btc_input.amount
-    // Create BTC and USDT outputs with swapped amounts
-    #btc_output = { "asset": "BTC", "amount": #usdt_input.amount, "issuer": #btc_input.issuer, "owner": #btc_input.owner }
-    #usdt_output = { "asset": "USDT", "amount": #btc_input.amount, "issuer": #usdt_input.issuer, "owner": #usdt_input.owner }
-    // Return the outputs and the exchange rate
-    { "btc_output": #btc_output, "usdt_output": #usdt_output, "rate": #rate }
-OP_ENDIF
+use rgb::contract::Contract;
+use rgb::contract::OwnedState;
+use rgb::contract::OutpointCoins;
+use rgb::contract::OutpointHash;
+use rgb::contract::Transition;
+use rgb::contract::TransitionArgument;
+use rgb::contract::TransitionType;
+
+// Define the contract state
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct SwapContractState {
+    btc_input: Option<OutpointCoins>,
+    usdt_input: Option<OutpointCoins>,
+}
+
+// Implement the owned state trait for the contract state
+impl OwnedState for SwapContractState {
+    fn owner(&self) -> Option<OutpointHash> {
+        // The owner of the contract is the owner of the BTC input
+        self.btc_input.as_ref().map(|input| input.owner.clone())
+    }
+}
+
+// Define the swap contract
+struct SwapContract;
+
+// Implement the contract trait for the swap contract
+impl Contract for SwapContract {
+    type State = SwapContractState;
+
+    fn validate_transition(&self, transition: &Transition<Self::State>) -> Result<(), String> {
+        // Check that the transition type is valid
+        if transition.transition_type != TransitionType::Direct {
+            return Err("Invalid transition type".to_string());
+        }
+
+        // Check that the BTC and USDT inputs are present
+        let btc_input = transition
+            .input_states
+            .get(0)
+            .ok_or_else(|| "BTC input not found".to_string())?;
+        let usdt_input = transition
+            .input_states
+            .get(1)
+            .ok_or_else(|| "USDT input not found".to_string())?;
+
+        // Check that the BTC input amount is greater than 0
+        if btc_input.coins.amount == 0 {
+            return Err("BTC input amount must be greater than 0".to_string());
+        }
+
+        // Check that the USDT input amount is greater than 0
+        if usdt_input.coins.amount == 0 {
+            return Err("USDT input amount must be greater than 0".to_string());
+        }
+
+        // Check that the BTC and USDT inputs have the same issuer
+        if btc_input.coins.issuer != usdt_input.coins.issuer {
+            return Err("BTC and USDT inputs have different issuers".to_string());
+        }
+
+        // Check that the BTC and USDT inputs have different owners
+        if btc_input.owner == usdt_input.owner {
+            return Err("BTC and USDT inputs have the same owner".to_string());
+        }
+
+        // Store the BTC and USDT inputs in the contract state
+        let state = Self::State {
+            btc_input: Some(btc_input.coins),
+            usdt_input: Some(usdt_input.coins),
+        };
+        transition.output_state = state.clone();
+
+        Ok(())
+    }
+
+    fn validate_transition_argument(
+        &self,
+        _state: &Self::State,
+        _transition_type: &TransitionType,
+        _argument: &TransitionArgument,
+    ) -> Result<(), String> {
+        // There are no arguments to validate for this contract
+        Ok(())
+    }
+
+    fn get_owned_state(&self, state: &Self::State) -> Option<OwnedState> {
+        // Return the contract state as the owned state
+        Some(state.clone())
+    }
+}
+
