4c7c10d338e7eaa2e211b0ede77beb5a85c208e3
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-14 11:50:26 -0300

    Update node.rs

--- rgb/node.rs
+++ rgb/node.rs
@@ -1,1 +1,6 @@
+let btc_contract_id = ContractId::from_hex("YOUR_BTC_CONTRACT_ID").expect("Invalid contract ID");
+let usdt_contract_id = ContractId::from_hex("YOUR_USDT_CONTRACT_ID").expect("Invalid contract ID");
+
+node.register::<Asset>(btc_contract_id, Asset::BTC);
+node.register::<Asset>(usdt_contract_id, Asset::USDT);
 
