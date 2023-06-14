c30dea1b15dc8df9718e21ded2bd906a36469d3d
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-14 11:50:07 -0300

    Update pair.rs

--- src/dex/pair.rs
+++ src/dex/pair.rs
@@ -1,1 +1,29 @@
+use rgb::traits::Validate;
+use rgb::value::Value;
+use rgb::{ContractId, Consignment, Genesis, Node, NodeId};
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+enum Asset {
+    BTC,
+    USDT,
+}
+
+impl Validate for Asset {
+    fn is_valid(_: &ContractId, _: &Value) -> bool {
+        true
+    }
+}
+impl Genesis for Asset {
+    fn genesis(_: &ContractId, _: &Value) -> Consignment {
+        Consignment::Issue {
+            txid: None,
+            issuance_amount: 0,
+            inflation_keys: vec![],
+        }
+    }
+}
+fn main() {
+    let node_id = NodeId::from_hex("YOUR_NODE_ID").expect("Invalid node ID");
+    let mut node = Node::new(node_id);
+}
 
