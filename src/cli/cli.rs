bcae8db94d3330cc93af09293319659de3cfd4c7
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-07-12 09:04:35 -0300

    Create cli.rs

---
+++ src/cli/cli.rs
@@ -0,0 +1,69 @@
+use ldk::wallet::Wallet;
+use rgb::core::contract::Contract;
+use rgb::core::fungible::{Mint, OutpointValue, ValueSum};
+use rgb::core::issue::Amount;
+use std::error::Error;
+use std::io::{self, Write};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Initialize RGB Core and LDK
+
+    // Your code here
+
+    // Create a new RGB contract instance
+    let contract = Contract::new();
+
+    // Create a new wallet instance
+    let mut wallet = Wallet::new();
+
+    loop {
+        // Display the command prompt
+        print!("> ");
+        io::stdout().flush()?;
+
+        // Read the user input
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        // Parse the user input
+        let args: Vec<&str> = input.trim().split(' ').collect();
+
+        // Check the command and execute the corresponding action
+        match args[0] {
+            "swaps" => {
+                // Perform swaps
+                // Your code here
+            }
+            "mint" => {
+                // Mint new RGB assets
+                // Your code here
+            }
+            "enable_channel" => {
+                // Enable an open channel
+                // Your code here
+            }
+            "disable_channel" => {
+                // Disable a close channel
+                // Your code here
+            }
+            "enter_liquidity" => {
+                // Enter liquidity for RGB assets or Bitcoin
+                // Your code here
+            }
+            "exit_liquidity" => {
+                // Exit liquidity for RGB assets or Bitcoin
+                // Your code here
+            }
+            "exit" => {
+                // Exit the program
+                break;
+            }
+            _ => {
+                // Invalid command
+                println!("Invalid command");
+            }
+        }
+    }
+
+    Ok(())
+}
