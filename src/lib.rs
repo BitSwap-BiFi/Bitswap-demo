4c42cc55dc2ac54b3e5936b29cdeebd4db89463d
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-07-25 07:07:34 -0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,42 @@
+//! # Bitswap Core
+//! `bitswap core`integration with DEX on RGB protocol
+/// 
+///
+///```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+//! # Art
+//!
+//! A library for modeling artistic concepts.
 
+pub mod kinds {
+    /// The primary colors according to the RYB color model.
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// The secondary colors according to the RYB color model.
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils {
+    use crate::kinds::*;
+
+    /// Combines two primary colors in equal amounts to create
+    /// a secondary color.
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        // --snip--
+    }
+}
