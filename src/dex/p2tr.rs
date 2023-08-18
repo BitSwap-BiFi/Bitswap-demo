8b9577f050b72cf98b0da72989f96e7c3bbef74a
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-18 09:33:11 -0300

    Update p2tr.rs

--- src/dex/p2tr.rs
+++ src/dex/p2tr.rs
@@ -1,56 +1,25 @@
-// Rust code for integrating Taproot (P2TR) with RGB Node, AMM algorithm, DLCs, and Payjoin
+
+use std::borrow::{Borrow, BorrowMut};
+use std::cmp::Ordering;
+use std::fmt::{self, Debug, Display, Formatter};
+use std::io::{Read, Write};
+use std::ops::{Deref, Not};
+use std::str::FromStr;
 
 use rgb_node::{Daemon, Mod, Opts, Service};
 use bitswap_core::{Aluvm,PayJoin,Swap};
 use rust_dlc::{Message};
 use rust_payjoin::{Sender, Receiver, Input, Output};
-use rust_p2tr::{Input, Output};
+use amplify::Wrapper;
+use bitcoin::hashes::Hash;
+use bitcoin::psbt::TapTree;
+use bitcoin::util::taproot::{LeafVersion, TapBranchHash, TapLeafHash, TaprootBuilder};
+use bitcoin::Script;
+use strict_encoding::{StrictDecode, StrictEncode}
 
-fn main() {
-    // Initialize RGB Node
-    let rgb_node = RGBNode::new("your_rgb_node_settings");
 
-    // Initialize AMM algorithm
-    let amm_algorithm = AutomatedMarketMaker::new("your_amm_algorithm_settings");
+use crate::types::IntoNodeHash;
+use crate::{LeafScript, TapNodeHash, TapScript};
 
-    // Initialize DLCs
-    let dlcs = DiscreetLogContracts::new("your_dlcs_settings");
+impl TaprootScriptTree
 
-    // Initialize Payjoin
-    let payjoin = Payjoin::new("your_payjoin_settings");
-
-    // Initialize Taproot
-    let taproot = Taproot::new("your_taproot_settings");
-
-    // Connect AMM algorithm to RGB Node
-    amm_algorithm.connect_to_rgb_node(&rgb_node);
-
-    // Connect DLCs to RGB Node
-    dlcs.connect_to_rgb_node(&rgb_node);
-
-    // Connect Payjoin to RGB Node
-    payjoin.connect_to_rgb_node(&rgb_node);
-
-    // Connect Taproot to RGB Node
-    taproot.connect_to_rgb_node(&rgb_node);
-
-    // Perform operations using the integrated components
-
-    // Simulate AMM swap
-    let btc_to_swap = 5;
-    let amm_result = amm_algorithm.swap(btc_to_swap);
-    println!("AMM swap result: {:?}", amm_result);
-
-    // Simulate DLC offer
-    let dlc_contract_id = "dlc_contract_id";
-    let dlc_result = dlcs.create_offer(dlc_contract_id);
-    println!("DLC offer result: {:?}", dlc_result);
-
-    // Simulate Payjoin transaction
-    let payjoin_result = payjoin.create_transaction("payjoin_address", "transaction_data");
-    println!("Payjoin transaction result: {:?}", payjoin_result);
-
-    // Simulate Taproot transaction
-    let taproot_result = taproot.create_transaction("taproot_address", "transaction_data");
-    println!("Taproot transaction result: {:?}", taproot_result);
-}
