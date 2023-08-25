0e4b3970471aedfd1b75daa97fdc8173d0442912
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-25 06:42:55 -0300

    Update algorithm.rs

--- src/dex/algorithm.rs
+++ src/dex/algorithm.rs
@@ -1,4 +1,24 @@
-use rgb_core::{Contract, ExecutionResult};
+// Bitswap Core
+//
+// SPDX-License-Identifier: Apache-2.0, MIT LICENSE, GNU General Public License version 3
+//
+// Written in 2023 by Rsync and 22388O
+//
+// Copyright (C) 2023 Bitswap. All rights reserved.
+// Copyright (C) 2023 Rsync and 22388O. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.use rgb_core::{Contract, ExecutionResult};
+
 use aluvm::{Executor, Value};
 
 struct AmmContract {
