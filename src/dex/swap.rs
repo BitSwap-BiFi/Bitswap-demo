15828c95f925575320146a5a05af6a999fe43c63
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-09-11 06:39:48 -0300

    Update swap.rs

--- src/dex/swap.rs
+++ src/dex/swap.rs
@@ -30,6 +30,8 @@ fn create_swap() {
     let swap_fee = 0.05;
     let fee_spread = 0.1;
     let psbt = psbt();
+    let tapr_accepted = tapr();
+
 }
 
 fn swap_accepted() {
@@ -41,6 +43,7 @@ fn swap_accepted() {
     let fee_spread_accepted = 0.1;
     let dlc_accepted = secp256k1_zkp();
     let psbt_accepted = psbt();
+    let tapr_accepted = tapr();
 }
 
 fn swp_out() {
