ffbd9e7b0306557838bdbc3f4ada2b89bd589b13
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-10-12 06:32:24 -0300

    Update asset.rs

--- src/contract/asset.rs
+++ src/contract/asset.rs
@@ -3,7 +3,7 @@ use std::baid58;
 use std::amplify;
 
 
-use rgb_core::{Consignment, Contract, Error, ExecutionResult, Schema, Value};
+use rgb_core::{Consignment, Contract, Error, ExecutionResult, Schema, Value, State};
 use baid58::{Baid58ParseError, Chunking, FromBaid58, ToBaid58, CHUNKING_32};
 use amplify::{ByteArray, Bytes32};
 use std::collections::HashMap;
