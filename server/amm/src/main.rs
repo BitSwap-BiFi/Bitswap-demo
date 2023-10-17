c183be8eded2e7d36f2efb648ffd4256e7783e5e
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-10-17 07:21:46 -0300

    Update main.rs

--- server/amm/src/main.rs
+++ server/amm/src/main.rs
@@ -1,1 +1,42 @@
+use std::rgb_core;
+use std::dlc;
+use std::rgb_std;
 
+use rgb_core::schema;
+use rgb_std::{fungible::Amount, seal::seal_confidential};
+
+pub(crate) type DlcManager = dlc_manager::manager::Manager<
+    Arc<BitcoinCoreProvider>,
+    Arc<BitcoinCoreProvider>,
+    Box<dlc_sled_storage_provider::SledStorageProvider>,
+    Arc<SystemTimeProvider>,
+    Arc<BitcoinCoreProvider>,
+    // Add RGB-related fields here
+    schema::Schema,
+    schema::SchemaId,
+    schema::Genesis,
+    schema::SchemaAnchor,
+    Amount,
+    seal_confidential::Witness,
+>;
+let dlc_manager: Arc<Mutex<DlcManager>> = Arc::new(Mutex::new(
+    DlcManager::new(
+        bitcoind_provider.clone(),
+        bitcoind_provider.clone(),
+        Box::new(
+            dlc_sled_storage_provider::SledStorageProvider::new(&config.datadir)
+                .expect("Error creating storage."),
+        ),
+        oracles,
+        Arc::new(dlc_manager::SystemTimeProvider {}),
+        bitcoind_provider.clone(),
+        // Add RGB-related parameters here
+        Schema::new(),
+        SchemaId::new(),
+        Genesis::new(),
+        SchemaAnchor::new(),
+        Amount::from_sat(0),
+        seal_confidential::Witness::default(),
+    )
+    .unwrap(),
+));
