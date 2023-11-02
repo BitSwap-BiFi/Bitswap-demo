29e4ba2536702195be56a50761f6b7655dd77cfd
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-11-02 14:58:32 -0300

    Update lib.rs

--- src/dex/functions/main/lib.rs
+++ src/dex/functions/main/lib.rs
@@ -1,1 +1,19 @@
+pub mod cfmm;
+pub mod cost_function;
+pub mod utils;
 
+pub mod dto;
+pub mod entity;
+
+use amplify::{From, Wrapper};
+use noisy_float::types::R64;
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, From, Wrapper)]
+pub struct AssetId([u8; 32]);
+
+#[derive(Clone, Debug, PartialEq, PartialOrd)]
+pub struct AssetInfo {
+    id: AssetId,
+    amount: R64,
+    ticker: String,
+}
