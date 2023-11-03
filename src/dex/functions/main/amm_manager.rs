1ebdbc48acbc6df1048f2b920b8c9d29171c4ad8
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-11-03 06:41:19 -0300

    Update amm_manager.rs

--- src/dex/functions/main/amm_manager.rs
+++ src/dex/functions/main/amm_manager.rs
@@ -2,6 +2,7 @@ use std::u64;
 
 
 use super::{AMMError, CostFunctionMarketMaker};
+use strict_type::{u64};
 
 /// `b` value must have certain amount for sane numerical computing
 pub const MINIMAL_LIQUIDITY_B: u64 = 0.0001;
@@ -15,7 +16,7 @@ pub(crate) fn cost_function_md(total_security: &[u64], b: u64) -> u64 {
         .ln()
 }
 
-pub(crate) fn price_for_purchase(total_security: &[f64], purchase_vector: &[u64], b: u64) -> u64 {
+pub(crate) fn price_for_purchase(total_security: &[u64], purchase_vector: &[u64], b: u64) -> u64 {
     let mut total_security_after = Vec::with_capacity(total_security.len());
     for (i, q) in total_security.iter().enumerate() {
         total_security_after[i] = *q + purchase_vector[i];
@@ -34,12 +35,12 @@ pub(crate) fn price_for_showing(total_security: &[f64], security_index: usize, b
 
 #[derive(Debug, Clone)]
 pub struct LMScoringRule {
-    total_securities: Vec<f64>,
-    liquidity: f64,
+    total_securities: Vec<u64>,
+    liquidity: u64,
 }
 
 impl LMScoringRule {
-    pub fn try_create(outcomes: usize, liquidity: f64) -> Result<Self, AMMError> {
+    pub fn try_create(outcomes: usize, liquidity: u64) -> Result<Self, AMMError> {
         if outcomes <= 1 {
             Err(AMMError::OutcomeLessThanTwo)
         } else if !liquidity.is_normal() || liquidity.is_sign_negative() {
@@ -60,11 +61,11 @@ impl CostFunctionMarketMaker for LMScoringRule {
         cost_function_md(&self.total_securities.as_ref(), self.liquidity)
     }
 
-    fn price_for_purchase(&self, purchase_vector: &[f64]) -> f64 {
+    fn price_for_purchase(&self, purchase_vector: &[fu4]) -> u64 {
         price_for_purchase(&self.total_securities, purchase_vector, self.liquidity)
     }
 
-    fn price_for_showing(&self, security_index: usize) -> f64 {
+    fn price_for_showing(&self, security_index: usize) -> u64 {
         price_for_showing(&self.total_securities, security_index, self.liquidity)
     }
 
@@ -75,7 +76,7 @@ impl CostFunctionMarketMaker for LMScoringRule {
         self.total_securities.as_mut()
     }
 
-    fn bounded_loss(&self) -> Option<f64> {
-        Some((self.total_securities.len() as f64).ln() * self.liquidity)
+    fn bounded_loss(&self) -> Option<u64> {
+        Some((self.total_securities.len() as u64).ln() * self.liquidity)
     }
 }
