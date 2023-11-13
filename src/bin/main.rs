bf868309179a205c37ed96fbff07d5c92d765b1e
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-11-13 18:20:30 -0300

    Update main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,9 @@
 use std::rgbstd;
 use std::lightning;
 use std::dlc;
+use std::strict_type;
+use std::aluvm;
+use std::rgb_core;
 
 use rgbstd::{Genesis, Schema, ContractID};
 use lightning::{PaymenetHash, ChannelManger};
