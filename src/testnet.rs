f65c75e8dd34c3f5e04b7767e2c084274213bcc4
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-11-16 16:14:17 -0300

    Update testnet.rs

--- src/testnet.rs
+++ src/testnet.rs
@@ -1,3 +1,14 @@
-use std::bitcoin;
+use bitcoin::network::constants::Network;
+use bitcoin::util::address::Address;
+use bitcoin::network::constants::Network::Testnet;
+use rgbstd::{InvoiceState};
+
+fn main() {
+    let address = Address::from_str("," Testnet).unwrap();
+    println!("Address: {}", address);
+    let rgb_std = Address::from_str("," Testnet).unwrap();
+       println!("Address: {}", address);
+   
+}
 
 
