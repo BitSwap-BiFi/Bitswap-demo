9a859f75c84cb77dbe7d93b24fdac57b4d7855d5
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-12-07 06:46:46 -0300

    Update op.rs

--- src/op/op.rs
+++ src/op/op.rs
@@ -1,7 +1,10 @@
 use rgb_core::vm::op_timechain;
 use rgb_core::vm::op_contract;
 use rgb_core::vm::opcodes;
+use aluvm::isa::opcodes::INSTR_ISAE_TO;
 
+impl opcode = {
+  let  INSTR_CNP: u8 = 0b11_000_000;
 
 
 
