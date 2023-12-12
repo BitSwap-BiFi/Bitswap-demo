8955b5d15ba8b81d0ac4cf5c9b0f054797f05fc9
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-12-12 14:02:43 -0300

    Update lsp.rs

--- src/lightning/lsp.rs
+++ src/lightning/lsp.rs
@@ -1,1 +1,50 @@
+use lightning::chain::keysinterface::KeysInterface;
+use lightning::ln::chan_utils::ChannelPublicKeys;
+use lightning::ln::channelmanager::ChannelManager;
+use lightning::ln::msgs::ChannelMessageHandler;
+use rgb_core::rgb::RgbMessageHandler;
+
+struct MyKeysInterface;
+
+impl KeysInterface for MyKeysInterface {
+    // Implement the required methods for KeysInterface
+    // ...
+
+    fn get_secure_random_bytes(&self, _dest: &mut [u8]) {
+        // Implement secure random bytes generation
+        // ...
+    }
+}
+
+struct MyChannelMessageHandler;
+
+impl ChannelMessageHandler for MyChannelMessageHandler {
+    // Implement the required methods for ChannelMessageHandler
+    // ...
+}
+
+struct MyRgbMessageHandler;
+
+impl RgbMessageHandler for MyRgbMessageHandler {
+    // Implement the required methods for RgbMessageHandler
+    // ...
+}
+
+fn main() {
+    // Initialize your custom implementations
+    let keys_interface = MyKeysInterface;
+    let channel_message_handler = MyChannelMessageHandler;
+    let rgb_message_handler = MyRgbMessageHandler;
+
+    // Create a ChannelManager instance
+    let mut channel_manager = ChannelManager::new(
+        keys_interface,
+        channel_message_handler,
+        rgb_message_handler,
+        // ... other parameters ...
+    );
+
+    // Start your Lightning Service Provider
+    // ...
+}
 
