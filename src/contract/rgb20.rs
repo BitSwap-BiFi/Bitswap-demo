016d51507833a8fe579671d4c26981310bf213ab
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-01-04 16:26:22 -0300

    Update rgb20.rs

--- src/contract/rgb20.rs
+++ src/contract/rgb20.rs
@@ -1,6 +1,11 @@
 use rgbstd::interface::rgb20;
 use rgbstd::interface::{Amount, ContractID, Fungible, Divisible};
 
-impl rgb20interface = {
+impl rgb20::interface {
+    // Add your implementations here
+}
 
-impl rgbscehemata = {
+impl rgb20::schemata {
+    // Add your implementations here
+}
+
