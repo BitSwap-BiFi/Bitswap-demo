95c963a8032d7f26bab30f107b5b44a9c883fa6b
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-01-04 16:31:53 -0300

    Update rgb20.rs

--- src/contract/rgb20.rs
+++ src/contract/rgb20.rs
@@ -1,11 +1,14 @@
 use rgbstd::interface::rgb20;
 use rgbstd::interface::{Amount, ContractID, Fungible, Divisible};
+use rgbstd::
 
 impl rgb20::interface {
     // Add your implementations here
+    fn nia_schema (&subschema)
 }
 
 impl rgb20::schemata {
     // Add your implementations here
+    fn schemata(&schemata)
 }
 
