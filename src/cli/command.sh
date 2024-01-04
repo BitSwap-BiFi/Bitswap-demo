ab4374aba2524bd7a9e642b1f9c3862902de7c1a
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-01-04 06:46:40 -0300

    Fix command line in Sheell

--- src/cli/command.sh
+++ src/cli/command.sh
@@ -27,31 +27,55 @@ function perform_swaps() {
 
 # Function to enable an open channel
 function enable_channel() {
-    # Your code here
+    tem=$var1
+    var1=$var2
+    var2=$temp
+    price=$temp
+    channel=$temp
     echo "Enabling channel..."
 }
 
 # Function to disable a close channel
 function disable_channel() {
-    # Your code here
+    tem=$var1
+    var1=$var2
+    var2=$temp
+    price=$temp
+    disable_channel=$temp
     echo "Disabling channel..."
 }
 
 # Function to enter liquidity for RGB assets or Bitcoin
 function enter_liquidity() {
-    # Your code here
-    echo "Entering liquidity..."
-}
+    tem=$var1
+    var1=$var2
+    var2=$temp
+    price=$temp
+    liquidity=$temp
+[    echo "Entering liquidity..."}
 
 # Function to exit liquidity for RGB assets or Bitcoin
 function exit_liquidity() {
-    # Your code here
+    tem=$var1
+    var1=$var2
+    var2=$temp
+    price=$temp
+    exit_liquidity=$temp
     echo "Exiting liquidity..."
 }
 
 # Function to check DLCs prices
 
 function oracle_price() {
+    tem=$var1
+    var1=$var2
+    var2=$temp
+    price=$temp
+    oracle=$temp
+   echo "Oracle Price..."
+
+}
+   
 
 while true; do
     # Display the command prompt
