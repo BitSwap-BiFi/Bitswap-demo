966444aadb6a38ba0c39af031c02de2cab9696ea
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-01-08 11:36:08 -0300

    Fix assets

--- src/contract/asset.rs
+++ src/contract/asset.rs
@@ -28,7 +28,6 @@ struct RGB20Asset {
     name: String,
     symbol: String,
     swap: String,
-    uxto: String,
     lp: String,
     schema: String,
     decimal: Number,
@@ -44,7 +43,6 @@ impl RGB20Asset {
             symbol,
             total_supply,
             decimal,
-            utxo,
             balances: HashMap::new(),
         }
     }
