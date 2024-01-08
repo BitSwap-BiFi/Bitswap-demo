bc57d784979232e3e7f36d8ee7a1f53be5a2d238
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-01-08 08:57:34 -0300

    Update rgb20.rs

--- src/contract/rgb20.rs
+++ src/contract/rgb20.rs
@@ -5,11 +5,17 @@ use rgb_schemata::{nia_rgb20, nia_schema};
 
 impl rgb20::interface {
     // Add your implementations here
-    fn rgb20 
-    fn nia_schema (&subschema)
-    fn if 
+    fn nia_schema (&subschema)  
+  fn nia_rgb20() -> IfaceImpl {
+    let schema = nia_schema();
+    let iface = rgb20();
         
-    
+   fn IfaceImpl {
+       let version: VerNo::V1,
+        let schema_id: schema.schema_id(),
+      let iface_id: iface.iface_id(),
+       let script: none!(),
+       let  global_state: tiny_bset! 
 }
 
 impl rgb20::schemata {
