8e26d72dc9d0801254ef1fb07f0c91617babefcc
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-01-08 15:00:32 -0300

    Fix AMM lib

--- server/amm/src/lib.rs
+++ server/amm/src/lib.rs
@@ -1,1 +1,17 @@
+// Bitswap
 
+// Bitswap core
+
+#[macro_use]
+extern crate api;
+#[macro_use]
+extern crate config;
+#[macro_use]
+extern crate main;
+#[macro_use]
+extern crate lib;
+
+mod api;
+mod config;
+mod main;
+mod lib;
