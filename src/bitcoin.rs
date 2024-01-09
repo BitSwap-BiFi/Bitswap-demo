9289f5b7647cedde4b58fb8110091f2cbfe0f2a9
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-01-09 07:56:48 -0300

    Update bitcoin.rs

--- src/bitcoin.rs
+++ src/bitcoin.rs
@@ -1,6 +1,12 @@
 
-use std:bp;
-use bp::psbt;
+use std:bp_core;
 use std::bp;
 use std::bitcoin;
+use bpstd::psbt;
+use bpstd::tapret;
 
+#[Derive]
+extern crate bitcoin;
+extern crate bp;
+extern crate psbt;
+extern crate tapret;
