73fc7520b5d6d0fd305d7138a10549fc6eeb01c7
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-01-10 13:33:00 -0300

    Update script.rs

--- src/op/script.rs
+++ src/op/script.rs
@@ -2,4 +2,7 @@ use rgb_core::Schema::Script;
 use rgb_core::vm::Script;
 
 fn script = {
-  let 
+  let ValidateGenesis =  ValidateGenesis::new();
+  let ValidateTransaction =  ValidateTransaction::new():
+  let  ValidateGlobalState = alidateGlobalState::new():
+  let ValidateOwnedState =  ValidateOwnedState::new();
