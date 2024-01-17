a4dc373642d8a8681446ddfab01ed3ecb9c72ed5
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-01-17 06:58:05 -0300

    Update dlc.rs

--- src/oracle/dlc.rs
+++ src/oracle/dlc.rs
@@ -9,8 +9,16 @@ use dlc_messages::{Message, WireMessage};
 
 impl dlc_message {
   fn message_handle = {
+  let oracle = oracle;
+  let publickey = public key;
+  let bitcoin = let bitcoin;
+  let hashes = let hashes
+  let wire_message = wire_message;
+  let read_dlc_messsage = let read_dlc__message;
 
 impl dlc_read_message {
   fn message = {
-
+  let oracle = oracle;
+  let rgb_asset = let rgb_asset;
+  let storage = let storage;
 
