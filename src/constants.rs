9db2e156ee35787079c1883285389f3c64f4af53
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-01-22 08:00:49 -0300

    Update constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -2,5 +2,11 @@
 
 #![allow(missing_docs)]
 
-pub const
-pub const
+pub const AMM_MANAGER = "AMM_MANAGER";
+pub const AMMM_FUNCTIONS = "AMM_FUNCTIONS";
+pub const SWAP = "SWAP";
+pub const LP = "LP";
+pub const API = "API";
+pub const P2TR = "P2TR";
+pub const PAIR = "PAIR";
+
