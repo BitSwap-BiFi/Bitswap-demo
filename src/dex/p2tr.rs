e0d58ba1f8ccd0831ca8dc639c0b66ffd6842472
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-01-25 06:32:08 -0300

    Update p2tr.rs

--- src/dex/p2tr.rs
+++ src/dex/p2tr.rs
@@ -6,12 +6,11 @@ use std::io::{Read, Write};
 use std::ops::{Deref, Not};
 use std::str::FromStr;
 
-use rgb_node::{Daemon, Mod, Opts, Service};
 use bitswap_core::{Aluvm,PayJoin,Swap};
-use rust_dlc::{Message};
-use rust_payjoin::{Sender, Receiver, Input, Output};
+use dlc::{Message};
+use payjoin::{Sender, Receiver, Input, Output};
 use amplify::Wrapper;
-use bitcoin::hashes::Hash;
+use bitcoi::hashes::Hash;
 use bitcoin::psbt::TapTree;
 use bitcoin::util::taproot::{LeafVersion, TapBranchHash, TapLeafHash, TaprootBuilder};
 use bitcoin::Script;
