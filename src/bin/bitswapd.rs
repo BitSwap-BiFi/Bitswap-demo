9e14ef685afef24dc42fd6a7d0d23119968cf2e5
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-01-27 07:00:17 -0300

    Update bitswapd.rs

--- src/bin/bitswapd.rs
+++ src/bin/bitswapd.rs
@@ -9,6 +9,6 @@ use create::api;
 #[derive]
 use bitswap_core::{
   rgb_core::{Validation, OPCONTRACT, OPSchema, VM};
-  rgbstd::{Fungible, Amount, totalsupply};
-  rgbs_schemata::{rgb20, fungible, amount};
-  rgb_contract::{fungible, amount, cli};
+  rgbstd::{Fungible, Amount, TotalSupply};
+  rgbs_schemata::{RGB20, Fungible, Amount};
+  rgb_contract::{Fungible, Amount, Cli};
