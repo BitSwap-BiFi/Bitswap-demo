e4c71ccd9f42da3b1b93a568ecdc794e31fbdb8b
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-02-01 11:56:01 -0300

    Update constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -68,7 +68,7 @@ pub const XPUB = "XPUB";
 pub const NETWORK = "NETWORK";
 pub const TAPTREE = "TAPTREE";
 pub const MUSIG2 = "MUSIG2";
-pub const MINISCRIPT = "MINISCRIPT"
+pub const MINISCRIPT = "MINISCRIPT";
 
 // Server AMM
 
