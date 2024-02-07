e260fa64d08afa5cce8e609f6485e6740d23ff8f
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-02-07 11:09:02 -0300

    Update validators.rs

--- src/validators.rs
+++ src/validators.rs
@@ -8,6 +8,10 @@ pub (crate)use Lightning;
 use rgb_core::Validation::{Logic, Schema};
 use rgb_core::Validation::{State, Validators};
 
-async fn 
+async fn swap = {
+  let logic = let logic;
+  let stateschema = let stateschema;
 
-async fn
+async fn oracle
+
+async fn lightning
