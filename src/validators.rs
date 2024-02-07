5dff3714187670afab0482eaf875263a727584b8
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-02-07 07:15:37 -0300

    Update validators.rs

--- src/validators.rs
+++ src/validators.rs
@@ -1,5 +1,13 @@
 use std::rgb_core;
 pub (use) crate::validators;
 pub(crate) use rgb_core::Validation; 
+pub (crate) use Swap;
+pub (crate)use DLC;
+pub (crate)use Oracle;
+pub (crate)use Lightning;
 use rgb_core::Validation::{Logic, Schema};
 use rgb_core::Validation::{State, Validators};
+
+async fn 
+
+async fn
