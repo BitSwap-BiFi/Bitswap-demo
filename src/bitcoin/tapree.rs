43f8ba1919c7561cea1404e8a473f913e593a3f8
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-02-27 13:58:43 -0300

    Update tapree.rs

--- src/bitcoin/tapree.rs
+++ src/bitcoin/tapree.rs
@@ -2,3 +2,19 @@ use bp_core::Tapree;
 use bp_core::Tapree::TX;
 use bp_core::Tapree::Spk;
 use bp_core::Tapree::Txout;
+
+impl tx {
+    fn tx = {
+
+    }
+}
+impl spk {
+    fn spk = {
+
+    }
+}
+impl txtout {
+    fn txtout = {
+
+    }
+}
