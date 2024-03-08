38bef2719ddaaa6dd1b8864450000640aca4a2e7
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-03-08 09:31:09 -0300

    Update proxy.rs

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,9 +1,12 @@
 use create::Proxy;
 
 use strict_encoding::StrictSerialize;
-
+   
 impl proxy {
     fn strict_encoding = {
-    let proxy 
+        let proxy = let proxy;
+        let server = let server;
+        let peer = let peeer;
+
     }
 }
