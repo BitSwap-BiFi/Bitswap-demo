c021f2a475bd77c0c615a4e947e642d8a30f04b3
223880 <rafael.segala45@gmail.com>
2024-06-21 07:39:50 -0300

    Add signet and regtest

--- src/testnet.rs
+++ src/testnet.rs
@@ -5,6 +5,8 @@ pub const BITCOIN_TESTNET = "BITCOIN_TESTNET";
 pub const TX = "TX";
 pub const XPUB = "XPUB";
 pub const RGB_ASSETS = "RGB_ASSETS";
+pub const BITCOIN_SIGNET = "BITCOIN_SIGNET";
+pub const BITCOIN_REGTEST = "BITCOIN_REGTEST";
 
 
 
