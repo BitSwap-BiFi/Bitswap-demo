3507fedfecffaf8402c4581feaf07a388cc91771
223880 <rafael.segala45@gmail.com>
2024-06-26 08:31:01 -0300

    Add swaps errors functions and impl

--- src/error.rs
+++ src/error.rs
@@ -19,3 +19,37 @@ fn main() {
         Err(err) => eprintln!("Error swap assets: {}", err),
     }
 }
+
+impl Swap {
+    pub fn open(path: &str) -> Result<Swap, io::Error> {
+        let file = File::open(path)?;
+        Ok(Swap(file))
+    }
+}
+
+impl Read for Swap {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.0.read(buf)
+    }
+}
+
+impl RGBAsssets {
+    pub fn new(path: &str) -> Result<RGBAsssets, io::Error> {
+        let mut swap = Swap::open(path)?;
+        let mut rgbassets = String::new();
+        swap.read_to_string(&mut rgbassets)?;
+        Ok(RGBAsssets(rgbassets))
+    }
+}
+
+impl Read for RGBAsssets {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.0.read(buf)
+    }
+}
+
+impl FromStr for RGBAsssets {
+    type Err = error::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(RGBAsssets(s.to_string()))
+    }
