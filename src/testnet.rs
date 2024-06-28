c0f7a1a4a0ed05006f6897ab41369c4a391e9d86
223880 <rafael.segala45@gmail.com>
2024-06-28 07:08:48 -0300

    Fix

--- src/testnet.rs
+++ src/testnet.rs
@@ -1,12 +1,12 @@
 // ! Bitswap core testnet
 
-pub const MEMPOOL = "MEMPOOL";
-pub const BITCOIN_TESTNET = "BITCOIN_TESTNET";
-pub const TX = "TX";
+pub const MEMPOOL = "MEMPOOL&str";
+pub const BITCOIN_TESTNET = "BITCOIN_TESTNET&str";
+pub const TX = "TX&str";
 pub const XPUB = "XPUB";
-pub const RGB_ASSETS = "RGB_ASSETS";
-pub const BITCOIN_SIGNET = "BITCOIN_SIGNET";
-pub const BITCOIN_REGTEST = "BITCOIN_REGTEST";
+pub const RGB_ASSETS = "RGB_ASSETS&str";
+pub const BITCOIN_SIGNET = "BITCOIN_SIGNET&str";
+pub const BITCOIN_REGTEST = "BITCOIN_REGTEST&str";
 
 
 
