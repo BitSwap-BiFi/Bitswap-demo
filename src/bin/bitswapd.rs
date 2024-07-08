daa6769e96d80811de0839e49a3e0e3a66963b39
223880 <rafael.segala45@gmail.com>
2024-07-08 11:00:35 -0300

    Fix

--- src/bin/bitswapd.rs
+++ src/bin/bitswapd.rs
@@ -1,26 +1,7 @@
-mod cli;
-
-use cli::{
-    Command,
-    Main,
-    Mod,
-    Cli
-};
-
-use bin::Bitswapd;
-
 pub struct Bitswapd;
 
-fn main() {
-    // Your main logic here
-    let mut cli = Cli::new();
-    let command = cli.parse();
-    match command {
-        Command::Main(Main::Bitswapd(Bitswapd::Start(mut args))) => {
-            // Your logic here
-            println!("{:?}", args);
-            println!("Starting bitswapd");
-
-        }
-    }  
+impl Bitswapd {
+    pub fn start(args: Vec<String>) {
+        println!("Starting bitswapd with args: {:?}", args);
+    }
 }
