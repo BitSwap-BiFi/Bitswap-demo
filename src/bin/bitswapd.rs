90ef13cb390e4a79fa741a4d8ade8e67cf547357
223880 <rafael.segala45@gmail.com>
2024-07-09 08:23:04 -0300

    Fix

--- src/bin/bitswapd.rs
+++ src/bin/bitswapd.rs
@@ -1,10 +1,13 @@
 
-use std::env;
-
 pub struct Bitswapd;
 
 impl Bitswapd {
     pub fn start(args: Vec<String>) {
         println!("Starting bitswapd with args: {:?}", args);
     }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    Bitswapd::start(args);
 }
