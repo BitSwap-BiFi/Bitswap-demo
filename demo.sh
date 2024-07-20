4d4bc332535f5b21c4dd60eb1e1d449f34f3fd0f
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-07-20 08:45:30 -0300

    Create demo.sh

---
+++ demo.sh
@@ -0,0 +1,8 @@
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install Cargo dependencies
+cargo install rgbstd
+cargo install ldk
+cargo install rgb
+cargo install rust
