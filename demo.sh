0ea9a0970461421c707d4754a58925d696d23392
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-07-21 09:53:49 -0300

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -4,7 +4,8 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # Install Cargo dependencies
 cargo install rgbstd
 cargo install rgb
+cargo install rgbcore
+cargo install bpstd
 cargo install rust
 cargo install rustdlc
 cargo install rustlightning
-cargo install rgbcore
