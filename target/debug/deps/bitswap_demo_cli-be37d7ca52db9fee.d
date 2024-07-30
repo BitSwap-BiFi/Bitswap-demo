921ae419f5885888c762a6918e364582f5963c5e
223880 <rafael.segala45@gmail.com>
2024-07-30 12:18:16 -0300

    fix

---
+++ target/debug/deps/bitswap_demo_cli-be37d7ca52db9fee.d
@@ -0,0 +1,5 @@
+/sysroot/home/rafael/Bitswap Demo CLI/Bitswap-demo/target/debug/deps/bitswap_demo_cli-be37d7ca52db9fee: src/main.rs
+
+/sysroot/home/rafael/Bitswap Demo CLI/Bitswap-demo/target/debug/deps/bitswap_demo_cli-be37d7ca52db9fee.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/bitswapd-78a9ad8d6c08a1cf.d
@@ -0,0 +1,5 @@
+/sysroot/home/rafael/Bitswap Demo CLI/Bitswap-demo/target/debug/deps/bitswapd-78a9ad8d6c08a1cf: src/bin/bitswapd.rs
+
+/sysroot/home/rafael/Bitswap Demo CLI/Bitswap-demo/target/debug/deps/bitswapd-78a9ad8d6c08a1cf.d: src/bin/bitswapd.rs
+
+src/bin/bitswapd.rs:
