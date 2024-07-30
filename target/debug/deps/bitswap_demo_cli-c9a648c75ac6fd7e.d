bcb8aa351aa67cf90d78be0a39df127982e8ef2d
223880 <rafael.segala45@gmail.com>
2024-07-30 10:04:47 -0300

    Add cargo

---
+++ target/debug/deps/bitswap_demo_cli-c9a648c75ac6fd7e.d
@@ -0,0 +1,5 @@
+/sysroot/home/rafael/Bitswap Demo CLI/Bitswap-demo/target/debug/deps/libbitswap_demo_cli-c9a648c75ac6fd7e.rmeta: src/main.rs
+
+/sysroot/home/rafael/Bitswap Demo CLI/Bitswap-demo/target/debug/deps/bitswap_demo_cli-c9a648c75ac6fd7e.d: src/main.rs
+
+src/main.rs:
